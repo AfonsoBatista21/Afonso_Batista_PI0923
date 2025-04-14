73d83b684fc125aad4e3c2055aff76b16ab9e411
Afonso Batista <actimeltheman@gmail.com>
2025-04-14 14:52:25 +0100

    Exe1_Py

---
+++ Python/primeiro.py
@@ -0,0 +1,37 @@
+
+#opc=0
+
+
+#print("Bem Vindo")
+#print("\nprima 1 para nome cliente")
+#print("\nprima 2 para IBAN")
+#opc = int(input("Intrud opção: "))
+
+#match opc:
+ #   case 1:
+  #      print("Nome")
+   # case 2:
+    #    print("IBAN")
+    #case default:
+     #   print("falhou opc")    
+
+
+num1=6
+num2=1
+num3=3
+
+if num1>num2 and num2>num3:
+     print("Numero 1 é o maior Numero 2 é o do meio Numero 3 é o menor")
+elif num1>num3 and num3>num2:
+     print("Num1 ser maior num3 ser meio num2 ser menor")
+elif num3>num2 and num2>num1:
+     print("Num3 ser maior num2 ser meio num1 ser menor")
+elif num3>num1 and num1>num2:
+     print("Num3 ser maior num1 ser meio num2 ser menor")
+elif num2>num3 and num3>num1:
+     print("num2 ser maior num 3 ser meio num1 ser menor")
+elif num2>num1 and num1>num3:
+     print("num2 ser maior num1 ser meio num3 ser menor")
+
+
+
