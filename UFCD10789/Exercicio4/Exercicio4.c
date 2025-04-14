9c3db9260a91f75b2bea06e954eed8a0300886d1
Afonso Batista <actimeltheman@gmail.com>
2025-04-14 14:27:48 +0100

    Exercicio 4

---
+++ UFCD10789/Exercicio4/Exercicio4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int saldo_inicial;
+    int cheque;
+
+    printf("Insira o seu saldo inicial: ");
+    scanf("%f", &saldo_inicial);
+
+    printf("Insira o valor do cheque: ");
+    scanf("%f", %cheque)
+
+    if(saldo_inicial > cheque){
+        printf("O seu saldo permite o desconto do cheque")
+    }
+
+
+
+
+
+}
