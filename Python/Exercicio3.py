f4b17459a5c15efa225994bcf066ef5436ca6560
AfonsoBatista21 <actimeltheman.gmail.com>
2025-04-15 15:04:58 +0100

    Exe3py

---
+++ Python/Exercicio3.py
@@ -0,0 +1,29 @@
+noms=[]
+
+while True:
+    print("1 - Introduzir Nomes")
+    print("\n2 - Listar Nomes")
+    print("\n3 - Parar Programa")
+    opc= int(input("Introduza um Num"))
+
+    match opc:
+            case 1:
+                print("1 - Introduzir Nomes")
+            case 2:
+                print("\n2 - Listar Nomes")
+            case 3:
+                break
+
+
+
+
+# para= True
+
+#while para:
+    #dec=input("Quer parar? S ou N: ")
+    #if dec == "S" or dec == "s":
+     #   para=False
+     #   break
+    #noms.append(input("Introduz novo nome"))
+
+
