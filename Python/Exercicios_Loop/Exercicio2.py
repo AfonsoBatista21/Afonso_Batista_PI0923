27408d2df2ba70508536f336477505dd5b3fec24
Afonso Batista <actimeltheman@gmail.com>
2025-04-22 15:03:35 +0100

    Exercicio Loop2

---
+++ Python/Exercicios_Loop/Exercicio2.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    numero = int(input(f"Digite o {i + 1}º número: "))
+    if numero % 2 == 0:
+        print(f"O número {numero} é par.")
+    else:
+        print(f"O número {numero} é ímpar.")
