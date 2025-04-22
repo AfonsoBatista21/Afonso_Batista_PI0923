eb261ed4b87e3027d5add23215d4fb6d951d1675
Afonso Batista <actimeltheman@gmail.com>
2025-04-22 13:35:50 +0100

    Exercico 6C

---
+++ UFCD10789/Exercicio6/Exercicio6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char cliente[100];
+    float valor_compra, desconto, apagar;
+
+
+    printf("Digite o nome do cliente: ");
+    fgets(cliente, sizeof(cliente), stdin);
+
+
+    printf("Digite o valor da compra: ");
+    scanf("%f", &valor_compra);
+
+
+    if (valor_compra <= 200)
+        desconto = (valor_compra * 10) / 100;
+    else if (valor_compra <= 500)
+        desconto = (valor_compra * 15) / 100;
+    else
+        desconto = (valor_compra * 20) / 100;
+
+
+    apagar = valor_compra - desconto;
+
+
+    printf("\nNome do cliente: %s", cliente);
+    printf("Valor da compra: %.2f\n", valor_compra);
+    printf("Valor do desconto: %.2f\n", desconto);
+    printf("Valor final da compra: %.2f\n", apagar);
+
+    return 0;
+}
