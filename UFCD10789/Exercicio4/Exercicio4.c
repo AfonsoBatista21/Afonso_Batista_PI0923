14ec3fd861495edd0dbe38f31c8c073e91484a4b
Afonso Batista <actimeltheman@gmail.com>
2025-04-22 12:37:40 +0100

    Exercicio 4 c

--- UFCD10789/Exercicio4/Exercicio4.c
+++ UFCD10789/Exercicio4/Exercicio4.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    int saldo_inicial;
-    int cheque;
+    float saldo_inicial;
+    float cheque;
 
     printf("Insira o seu saldo inicial: ");
     scanf("%f", &saldo_inicial);
 
     printf("Insira o valor do cheque: ");
-    scanf("%f", %cheque)
+    scanf("%f", &cheque);
 
     if(saldo_inicial > cheque){
-        printf("O seu saldo permite o desconto do cheque")
+        printf("O seu saldo permite o desconto do cheque");
+    }else{
+        printf("O seu saldo nao permite o desconto do cheque");
     }
 
-
-
-
+   printf("\n%f", saldo_inicial);
 
 }
