4ae682cabed8ca29c3ebfe598f3e0d89108e3ae5
AfonsoBatista21 <actimeltheman@gmail.com>
2025-04-23 09:54:04 +0100

    Update Exercicio2.c

--- UFCD10789/Exercicio2/Exercicio2.c
+++ UFCD10789/Exercicio2/Exercicio2.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-int max(int a, int b){
-    if(a>b){
+int max(int a, int b) {
+    if (a > b) {
         return a;
     }
     return b;
 }
+
 int max3(int a, int b, int c) {
-    return max(max(a,b),c);
+    return max(max(a, b), c);
 }
 
-int main(){
-    int num1;
-    int num2;
-    int num3;
-
-    printf("Insira o primeiro num: \n");
+int main() {
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    int maior =0;
+   
+    printf("Insira o primeiro número: \n");
     scanf("%d", &num1);
 
-    printf("Insira o segundo num: \n");
+    printf("Insira o segundo número: \n");
     scanf("%d", &num2);
 
-    printf("Insira o terceiro num: \n");
+    printf("Insira o terceiro número: \n");
     scanf("%d", &num3);
 
+    maior = max3(num1, num2, num3);
+    printf("O maior número é: %d\n", maior);
 
+    return 0;
 }
