0d18f8879bf0928ab0a03f59527211397cc9169c
AfonsoBatista21 <actimeltheman@gmail.com>
2025-04-23 09:56:20 +0100

    Correção Exercicio2.c

--- UFCD10789/Exercicio2/Exercicio2.c
+++ UFCD10789/Exercicio2/Exercicio2.c
@@ -7,16 +7,28 @@ int max(int a, int b) {
     return b;
 }
 
+int min(int a, int b) {
+    if (a < b) {
+        return a;
+    }
+    return b;
+}
+
 int max3(int a, int b, int c) {
     return max(max(a, b), c);
+}
+
+int min3(int a, int b, int c) {
+    return min(min(a, b), c);
 }
 
 int main() {
     int num1 = 0;
     int num2 = 0;
     int num3 = 0;
-    int maior =0;
-   
+    int maior = 0; 
+    int menor = 0; 
+
     printf("Insira o primeiro número: \n");
     scanf("%d", &num1);
 
@@ -27,7 +39,10 @@ int main() {
     scanf("%d", &num3);
 
     maior = max3(num1, num2, num3);
+    menor = min3(num1, num2, num3);
+
     printf("O maior número é: %d\n", maior);
+    printf("O menor número é: %d\n", menor);
 
     return 0;
 }
