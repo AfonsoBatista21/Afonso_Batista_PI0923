147987df2981f5c84c876e388be0d6fe683b14f4
AfonsoBatista21 <actimeltheman@gmail.com>
2025-04-23 09:42:51 +0100

    Update 2 Exercicio1.c

--- UFCD10789/Exercicio1/Exercicio1.c
+++ UFCD10789/Exercicio1/Exercicio1.c
@@ -3,15 +3,18 @@
 int main(){
 
     int segundos = 0;
-    int minutos = segundos/60;
-    segundos %= 60;
-    int horas = minutos/60;
-    minutos %= 60;
-    horas %= 24;
+    int minutos =0;
+    int horas =0;
+    
     
     printf("Insira o tempo em segundos: \n");
     scanf("%d", &segundos);
-
+    
+    minutos = segundos/60;
+    segundos %= 60;
+    horas = minutos/60;
+    minutos %= 60;
+    horas %= 24;
   
 
     printf("O seu tempo foi %d horas %d minutos %d segundos", horas, minutos, segundos);
