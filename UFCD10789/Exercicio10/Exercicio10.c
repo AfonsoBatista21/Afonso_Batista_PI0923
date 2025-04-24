6a9ef0221c173d6340046c8a9e0d7b923b281c67
AfonsoBatista21 <actimeltheman@gmail.com>
2025-04-24 10:43:17 +0100

    Update Exercicio10.c

--- UFCD10789/Exercicio10/Exercicio10.c
+++ UFCD10789/Exercicio10/Exercicio10.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int num;
+    int num=0;
 
 
     for (int i = 1; i <= 10; i++) {
-        printf("Digite o %d número: ", i);
-        scanf("%d", &numero);
+        printf("Digite o %d nÃºmero: ", i);
+        scanf("%d", &num);
 
         if (num % 2 == 0) {
             printf("O numero %d e PAR.\n", num;
